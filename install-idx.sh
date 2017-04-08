6dc104d94ec03c3bf01b55e85c518c2d98bea742
jansett <jansett@gmail.com>
2017-04-08 08:39:26 -0700

    Update install-idx.sh

--- install-idx.sh
+++ install-idx.sh
@@ -1,1 +1,36 @@
-
+#!/bin/sh
+  # Example file contents:
+  # splunkuser@50.18.226.85
+  HOSTS_FILE="SplunkCore"
+  WGET_CMD="wget -O splunk-6.5.3-36937ad027d4-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=6.5.3&product=splunk&filename=splunk-6.5.3-36937ad027d4-Linux-x86_64.tgz&wget=true'"
+  INSTALL_FILE="splunk-6.5.3-36937ad027d4-Linux-x86_64.tgz"
+  PASSWORD="SPLKp@ss"
+ 
+  REMOTE_SCRIPT="
+  cd /opt
+  sudo $WGET_CMD
+  sudo tar -xzf $INSTALL_FILE
+ 
+  sudo useradd -m -r splunk
+  sudo chown -R splunk:splunk /opt/splunk
+  echo $PASSWORD | sudo passwd splunk --stdin
+  sudo -u splunk /opt/splunk/bin/splunk start --accept-license --answer-yes --auto-ports --no-prompt
+  sudo -u splunk /opt/splunk/bin/splunk edit user admin -password $PASSWORD -auth admin:changeme
+  sudo -u splunk /opt/splunk/bin/splunk enable listen 9997 -auth admin:$PASSWORD
+  sudo -u splunk /opt/splunk/bin/splunk restart
+  sudo /opt/splunk/bin/splunk enable boot-start -user splunk
+  cd ~
+  sudo rm -f /opt/splunk-6.5.3-36937ad027d4-Linux-x86_64.tgz
+  "    
+ 
+  echo "Starting."
+  for DST in `cat "$HOSTS_FILE"`; do
+    if [ -z "$DST" ]; then
+      continue;
+    fi
+    echo "---------------------------"
+    echo "Installing to $DST"
+    sudo ssh -i SPLK.pem -t "$DST" "$REMOTE_SCRIPT"
+  done  
+  echo "---------------------------"
+  echo "Done"
